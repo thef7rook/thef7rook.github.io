bc56bda6b2f73d5f208f717929a63c3d981ebfe8
thef7rook <56137600+thef7rook@users.noreply.github.com>
2024-05-23 22:35:54 -0400

    Update Archiver.js

--- Archiver.js
+++ Archiver.js
@@ -1,5 +1,4 @@
 //This file exists in order to keep track of all the main data for everyone 
-archiver;
 const USERS_TO_TRACK = [Spartakusbund, TheF7Rook, JayTDemi, PineappleXXXL, SkyrorYaotzun, mamamarie21]
 function Archiver(){
 	this.peopleBeingTracked = [];  
